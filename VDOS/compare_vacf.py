7e269cba61155de41b863afbdf4e108e081f298a
Shahid <shahid@shahid.wifi.local.cmu.edu>
2026-08-13 23:44:21 -0400

    Add sPP VDOS data and plotting updates

--- VDOS/compare_vacf.py
+++ VDOS/compare_vacf.py
@@ -1,116 +1,112 @@
-########################### Code Overview #####################################
-# This script compares the Vibrational Density of States (VDOS) for different
-# polymer systems by plotting single-chain (sc) and crystalline (cryst) spectra.
-#
-# The VDOS data should already be generated using the VACF → FFT procedure
-# (see the VDOS generation script in this repository).
-#
-# Workflow:
-#   1. Load VDOS data files for both single-chain and crystalline systems.
-#   2. Remove small negative values (numerical FFT artifacts).
-#   3. Normalize the VDOS using the integral of the spectrum.
-#   4. Limit the frequency range to a specified maximum (freq_lim).
-#   5. Smooth the spectrum using a Savitzky–Golay filter to reduce noise.
-#   6. Plot and save the comparison figure.
-#
-# -------------------------------------------------------------------------------
-# Expected input files
-#
-# vdos_300K_sc_<polymer>.dat
-# vdos_300K_cryst_<polymer>.dat
-#
-# Example:
-#   vdos_300K_sc_sPP.dat
-#   vdos_300K_cryst_sPP.dat
-#
-# File format:
-#   Column 1 : Frequency (THz)
-#   Column 2 : VDOS
-#
-# -------------------------------------------------------------------------------
-# Output
-#
-# For each polymer type, the script saves a figure:
-#
-#   vdos_<polymer>_<freq_lim>.png
-#
-# Example:
-#   vdos_sPP_20.png
-#
-# The plot compares:
-#   - Black line  : Single-chain VDOS
-#   - Colored line: Crystalline VDOS
-#
-# Polymer color convention used in this work:
-#   PP systems → firebrick
-#   PS systems → teal
-#
-# -------------------------------------------------------------------------------
-# Polymers currently analyzed:
-#
-#   sPP : syndiotactic polypropylene
-#   iPP : isotactic polypropylene
-#   sPS : syndiotactic polystyrene
-#   iPS : isotactic polystyrene
-#
-###############################################################################
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
-
-freq_lim = 20
+from scipy.ndimage import gaussian_filter1d
 
 # Load VDOS data
-def vdos(polymername, freq_lim):
-    sc_data = np.loadtxt('vdos_300K_sc_%s.dat'%polymername, skiprows=1)
-    cr_data = np.loadtxt('vdos_300K_cryst_%s.dat'%polymername, skiprows=1)
+# spp_1c40 = np.loadtxt('vdos_300K_1c40.dat', skiprows=1)
+# spp_2c40 = np.loadtxt('vdos_300K_2c40.dat', skiprows=1)
+# spp_2d40 = np.loadtxt('vdos_300K_2d40.dat', skiprows=1)
+# spp_3d40 = np.loadtxt('vdos_300K_3d40.dat', skiprows=1)
 
-    # Extract frequency and VDOS
-    freq_sc = sc_data[:, 0]
-    freq_cr = cr_data[:, 0]
-    
-    sc = sc_data[:, 1]
-    cr = cr_data[:, 1]
+# spp_1c80 = np.loadtxt('vdos_300K_1c80.dat', skiprows=1)
+# spp_2c80 = np.loadtxt('vdos_300K_2c80.dat', skiprows=1)
+# spp_2d80 = np.loadtxt('vdos_300K_2d80.dat', skiprows=1)
+# spp_3d80 = np.loadtxt('vdos_300K_3d80.dat', skiprows=1)
 
-    sc[sc < 0] = 0
-    cr[cr < 0] = 0
+#spp_1c120 = np.loadtxt('vdos_300K_1c120.dat', skiprows=1)
+#spp_2c120 = np.loadtxt('vdos_300K_2c120.dat', skiprows=1)
+#spp_2d120 = np.loadtxt('vdos_300K_2d120.dat', skiprows=1)
+#spp_3d120 = np.loadtxt('vdos_300K_3d120.dat', skiprows=1)
 
-    sc /= np.trapz(sc, freq_sc)
-    cr /= np.trapz(cr, freq_cr)
+# Extract frequency and VDOS
 
-    mask_sc = freq_sc <= freq_lim
-    mask_cr = freq_cr <= freq_lim
-
+def get_vdos(filename):
+    spp_sc_data = np.loadtxt(filename, skiprows=1)
+    freq_sc = spp_sc_data[:, 0]
+    spp_sc = spp_sc_data[:, 1]
+    spp_sc[spp_sc < 0] = 0
+    spp_sc /= np.trapz(spp_sc, freq_sc)
+    mask_sc = freq_sc <= 20
     freq_sc = freq_sc[mask_sc]
-    freq_cr = freq_cr[mask_cr]
-    sc = sc[mask_sc]
-    cr = cr[mask_cr]
+    spp_sc = spp_sc[mask_sc]
+    spp_sc_smooth = savgol_filter(spp_sc, window_length=1501, polyorder=5)
+    return freq_sc, spp_sc_smooth
 
-    sc_smooth = savgol_filter(sc, window_length=2801, polyorder=5)
-    cr_smooth = savgol_filter(cr, window_length=2801, polyorder=5)
+# Plot
+plt.rcParams.update({
+    'font.family': 'Nimbus Roman',
+        'font.size': 20,
+    'axes.labelsize': 20,
+    'axes.titlesize': 20,
+    'xtick.labelsize': 20,
+    'ytick.labelsize': 20,
+    'legend.fontsize': 16,
 
-    if polymername =='iPS':
-        _ = 'teal'
-    elif polymername =='sPS':
-        _ = 'teal'
-    else:
-        _ = 'firebrick'
+    # ---- Tick styling ----
+    'xtick.direction': 'in',
+    'ytick.direction': 'in',
+    'xtick.major.size': 10,
+    'ytick.major.size': 10,
+    'xtick.major.width': 2.8,
+    'ytick.major.width': 2.8,
 
-    plt.figure()
-    plt.plot(freq_sc, sc_smooth, color='black', linewidth=2, label=' ')
-    plt.plot(freq_cr, cr_smooth, color=_, linewidth=2, label=' ')
-    plt.xlabel('Frequency (THz)')
-    plt.ylabel('VDOS (arb. units)')
-    plt.xlim(0, freq_lim)
-    plt.ylim(0, )
-    plt.yticks([])
-    plt.savefig('vdos_%s_%d.png'%(polymername, freq_lim), bbox_inches='tight', dpi=1000, transparent=True)
-    plt.close()
+    # ticks on all sides
+    'xtick.top': True,
+    'ytick.right': True,
+
+    # axis line thickness
+    'axes.linewidth': 1.8
+})
 
 
-polymers = ['sPP', 'iPP', 'sPS', 'iPS']
+spp_1c40freq, spp_1c40smooth  = get_vdos('vdos_300K_1c40.dat')
+spp_2c40freq, spp_2c40smooth = get_vdos('vdos_300K_2c40.dat')
+spp_2d40freq, spp_2d40smooth = get_vdos('vdos_300K_2d40.dat')
+spp_3d40freq, spp_3d40smooth = get_vdos('vdos_300K_3d40.dat')
+spp_1c80freq, spp_1c80smooth = get_vdos('vdos_300K_1c80.dat')
+spp_2c80freq, spp_2c80smooth = get_vdos('vdos_300K_2c80.dat')
+spp_2d80freq, spp_2d80smooth = get_vdos('vdos_300K_2d80.dat')
+spp_3d80freq, spp_3d80smooth = get_vdos('vdos_300K_3d80.dat')
+spp_1c120freq, spp_1c120smooth = get_vdos('vdos_300K_1c120.dat')
+spp_2c120freq, spp_2c120smooth = get_vdos('vdos_300K_2c120.dat')
+spp_2d120freq, spp_2d120smooth = get_vdos('vdos_300K_2d120.dat')
+spp_3d120freq, spp_3d120smooth = get_vdos('vdos_300K_3d120.dat')
 
-for p in polymers:
-    vdos(p, freq_lim)
+
+plt.figure()
+plt.plot(spp_1c40freq, spp_1c40smooth, color='red', linewidth=2, label='DP 40')
+plt.plot(spp_1c80freq, spp_1c80smooth, color='blue', linewidth=2, label='DP 80')
+plt.plot(spp_1c120freq, spp_1c120smooth, color='black', linewidth=2, label='DP 120')
+plt.xlabel('Frequency (THz)')
+plt.ylabel('VDOS (arb. units)')
+plt.xlim(0, 20)
+plt.ylim(0, )
+plt.yticks([])
+#plt.legend(frameon=False)
+plt.savefig('vdos_smooth_savgol_sPP_1chain.png', bbox_inches='tight', dpi=1000, transparent=True)
+plt.close()
+
+plt.figure()
+plt.plot(spp_1c40freq, spp_1c40smooth, color='red', linewidth=2, label='1 chain')
+plt.plot(spp_3d40freq, spp_3d40smooth, color='green', linewidth=2, label='Bulk')
+plt.xlabel('Frequency (THz)')
+plt.ylabel('VDOS (arb. units)')
+plt.xlim(0, 20)
+plt.ylim(0, )
+plt.yticks([])
+#plt.legend(frameon=False)
+plt.savefig('vdos_smooth_savgol_sPP_sc_cr_dp40.png', bbox_inches='tight', dpi=1000, transparent=True)
+plt.close()
+
+plt.figure()
+plt.plot(spp_1c120freq, spp_1c120smooth, color='black', linewidth=2, label='1 chain')
+plt.plot(spp_3d120freq, spp_3d120smooth, color='firebrick', linewidth=2, label='Bulk')
+plt.xlabel('Frequency (THz)')
+plt.ylabel('VDOS (arb. units)')
+plt.xlim(0, 20)
+plt.ylim(0, )
+plt.yticks([])
+#plt.legend(frameon=False)
+plt.savefig('vdos_smooth_savgol_sPP_sc_cr_dp120.png', bbox_inches='tight', dpi=1000, transparent=True)
+plt.close()
